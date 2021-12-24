c4579244ed994c83c71f2c1d6e5bbe746777052c
Nazar Dzhura <nazar_dzhura@ukr.net>
2021-12-24 19:27:30 +0200

    add: footer

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './footer.css'
+
+function Footer(){
+    return (
+        <footer className='footer'>
+            <a href='https://www.linkedin.com/in/nazar-dzhura/'>[in] Nazar Dzhura</a>,  SSI Test Task
+        </footer>
+    )
+}
+
+export default Footer
